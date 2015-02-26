9f5cfdb0d6291c10f57ded89ae5ed7b7c9096400
Sixto Martin <pitbulk@gmail.com>
2015-02-26 19:57:41 +0100

    Release the 2.1.1 version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='python-saml',
-    version='2.1.0',
+    version='2.1.1',
     description='Onelogin Python Toolkit. Add SAML support to your Python software using this library',
     classifiers=[
         'Development Status :: 4 - Beta',

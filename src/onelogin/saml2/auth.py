6107153fd6298cc146e6775b4d8db409f3b6c461
Krishna Kumar <kkumar@exathink.com>
2016-11-14 09:42:28 -0600

    Handle RelayState in SLO SAMLResponse

--- src/onelogin/saml2/auth.py
+++ src/onelogin/saml2/auth.py
@@ -139,6 +139,10 @@ class OneLogin_Saml2_Auth(object):
             elif not keep_local_session:
                 OneLogin_Saml2_Utils.delete_local_session(delete_session_cb)
 
+            if 'RelayState' in get_data:
+                return get_data['RelayState']
+
+
         elif get_data and 'SAMLRequest' in get_data:
             logout_request = OneLogin_Saml2_Logout_Request(self.__settings, get_data['SAMLRequest'])
             if not self.validate_request_signature(get_data):

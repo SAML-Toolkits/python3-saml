21a5b406aba7b5cf957573d6152d4f0ae374232f
Charles Beebe <charles@v-studios.com>
2017-03-24 11:02:18 -0400

    Add a Pyramid demo.
    Add a Pyramid demo in demo_pyramid dir (generated with
    'pyramid-cookiecutter-starter' Cookiecutter template).
    
    Add 'pyramid' as a keyword in setup.py.

---
+++ demo_pyramid/setup.py
@@ -0,0 +1,54 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'xmlsec',
+    'isodate',
+    'python3-saml',
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='demo_pyramid',
+    version='0.0',
+    description='demo_pyramid',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = demo_pyramid:main',
+        ],
+    },
+)

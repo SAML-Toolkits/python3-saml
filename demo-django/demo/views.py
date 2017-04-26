6a17663e17a3c6e779c1dea92fc33f7f1ff88ea3
Paul Albee <palbee@securityinnovation.com>
2017-04-26 15:52:05 -0700

    Removed deprecated import.

--- demo-django/demo/views.py
+++ demo-django/demo/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import (HttpResponse, HttpResponseRedirect,
                          HttpResponseServerError)
 from django.shortcuts import render_to_response

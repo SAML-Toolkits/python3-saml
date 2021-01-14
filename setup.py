615bb73452ddd714827f74efbbb0d1a94292f989
Sixto Martin <pitbulk@gmail.com>
2021-01-14 11:39:45 +0100

    Add reference to python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+	'Programming Language :: Python :: 3.8',
+	'Programming Language :: Python :: 3.9',
     ],
     author='OneLogin',
     author_email='support@onelogin.com',
